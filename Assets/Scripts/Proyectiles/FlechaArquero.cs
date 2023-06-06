1e2d31419b668da5f6a25ca155feb61ebf37f93f
DanielMayoralFB <d.mayoral.2019@alumnos.urjc.es>
2023-06-06 19:42:34 +0200

    Ataques básico y especial arquero
    Se ha implementado el ataque básico y el especial del arquero. De momento son flechas (capsulas rojas) que van rectas hasta que colisionan con algo que no sea otra flecha o el propio jugador.

---
+++ Assets/Scripts/Proyectiles/FlechaArquero.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlechaArquero : MonoBehaviour
+{
+
+    private int ataque;
+
+    public void SetAtaque(int ataque) { this.ataque = ataque; }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemigo"))
+        {
+            collision.gameObject.GetComponent<EnemigoController>().TakeDamage(ataque);
+        }
+
+        Destroy(gameObject);
+    }
+}
