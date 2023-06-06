31b9522d11483eb597f1bdc2f1f621aeedcbb509
DanielMayoralFB <d.mayoral.2019@alumnos.urjc.es>
2023-06-06 15:57:54 +0200

    Arreglo error spawn enemigos y mejora datos almacenados
    Se ha arreglado, en principio, un error en la posición Z del spawn de enemigos. Además, se ha cambiado algunos aspectos de la persistencia de datos, así como se ha añadido otros datos almacenados, el número de enemigos derrotados en la partida.

--- Assets/Scripts/Data/DataHandler.cs
+++ Assets/Scripts/Data/DataHandler.cs
@@ -6,6 +6,7 @@ using System;
 using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Linq;
+using TMPro;
 
 public class DataHandler : MonoBehaviour
 {
@@ -14,22 +15,31 @@ public class DataHandler : MonoBehaviour
     string path;
 
     SerializableScoreList scl;
+    [SerializeField] TextMeshProUGUI mostrarPuntuaciones;
 
 
     // METODOS
 
     public void Awake()
     {
-        path = Application.persistentDataPath;
+        path = Application.persistentDataPath + Path.DirectorySeparatorChar + "Puntuaciones.json";
 
-        if (File.Exists(path + "/Puntuaciones.json"))
+        if (File.Exists(path))
         {
-            string json = File.ReadAllText(path + "/Puntuaciones.json");
-            scl = JsonUtility.FromJson<SerializableScoreList>(json);
+            using (File.OpenRead(path))
+            {
+                string json = File.ReadAllText(path);
+                scl = JsonUtility.FromJson<SerializableScoreList>(json);
+            }
             //scl.list = scl.list.OrderByDescending(x => x.tiempo).ToList();
         } else
         {
             scl = new SerializableScoreList();
+
+            var fs = new FileStream(path, FileMode.Create);
+            fs.Dispose();
+            string txt = JsonUtility.ToJson(scl);
+            File.WriteAllText(path, txt);
         }
         Debug.Log(path);
     }
@@ -39,14 +49,49 @@ public class DataHandler : MonoBehaviour
         SerializableScore sc = new SerializableScore();
         sc.tiempo = GameManager.instance.GetTiempo();
         sc.ronda = GameManager.instance.GetRonda();
+        sc.enemigosDerrotados = GameManager.instance.GetEnemigosTotalesDerrotados();
 
         scl.list.Add(sc);
 
         scl.list = scl.list.OrderByDescending(x => x.ronda).ThenByDescending(x => x.tiempo).ToList();
 
-        String puntuacionesJson = JsonUtility.ToJson(scl);
+        string puntuacionesJson = JsonUtility.ToJson(scl);
 
-        File.WriteAllText(path + "/Puntuaciones.json", puntuacionesJson);
+        foreach(var s in scl.list)
+        {
+            Debug.Log(s);
+        }
+
+        File.WriteAllText(path, puntuacionesJson);
+    }
+
+    public void MostrarPuntuaciones()
+    {
+        if (!mostrarPuntuaciones.gameObject.activeSelf)
+        {
+
+            mostrarPuntuaciones.gameObject.SetActive(true);
+
+            SerializableScoreList scl = gameObject.GetComponent<DataHandler>().CargarPuntuaciones();
+
+            var texto = mostrarPuntuaciones.GetComponent<TextMeshProUGUI>();
+
+            foreach (SerializableScore sc in scl.list)
+            {
+                float minutos = Mathf.FloorToInt(sc.tiempo / 60);
+                float segundos = Mathf.FloorToInt(sc.tiempo % 60);
+
+                texto.text += sc.ronda + "\t \t \t" + string.Format("{0:0}:{1:00}", minutos, segundos) + "\n";
+            }
+        }
+    }
+
+    public void CerrarPuntuaciones()
+    {
+        var texto = mostrarPuntuaciones.GetComponent<TextMeshProUGUI>();
+        texto.text = "";
+
+        mostrarPuntuaciones.gameObject.SetActive(false);
     }
 
     public SerializableScoreList CargarPuntuaciones()
@@ -61,6 +106,8 @@ public class SerializableScore
     public float tiempo;
 
     public int ronda;
+
+    public int enemigosDerrotados;
 }
 
 [Serializable]
