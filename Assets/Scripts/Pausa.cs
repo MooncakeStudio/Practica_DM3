ada280648da27544f8f547ec84b280be0ea66775
Marta Rodríguez Castillo <martarc2001@hotmail.com>
2023-06-12 19:50:27 +0200

    Sonido partida

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    class Pausa : MonoBehaviour
+    {
+        
+        public void PauseJuego(bool pausar)
+        {
+            int pausaInt = pausar ? 0 : 1;
+            Time.timeScale = pausaInt;
+        }
+
+        public void SalirJuego()
+        {
+            GameManager.instance.resetGame();
+        }
+       
+
+    }
+}
